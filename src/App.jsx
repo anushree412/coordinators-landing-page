130b31f7fe48793112a90eb74518a726cad87e0d
anushree412 <anushreej412@gmail.com>
2024-04-15 18:48:23 +0530

    Coordinators landing page

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Header from './Components/Header'
+import Hero from './Components/Hero'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
